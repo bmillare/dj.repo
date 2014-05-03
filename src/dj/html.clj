50f313b68006206b4e75975577a59e92d573ff7f
Brent Millare <brent.millare@gmail.com>
2014-05-03 16:43:59 -0400

    new sgml data format

--- src/dj/html.clj
+++ src/dj/html.clj
@@ -2,14 +2,18 @@
   (:require [dj.sgml]))
 
 (defn html5 [content]
-  [{:!tag :DOCTYPE
-    :attrs "html"}
-   {:tag :html
-    :attrs {:lang "en"}
-    :content content}])
+  [[:!DOCTYPE "html"]
+   (apply vector :html {:lang "en"} content)])
 
-(defn base [content]
+#_ (defn base [content]
   (html5 (into [{:tag :head
                  :content [{:tag :meta
                             :attrs {:charset "UTF-8"}}]}]
                content)))
+
+(def basic
+  [[:!DOCTYPE "html"]
+   [:html {:lang "en"}
+    [:head nil
+     [:meta {:charset "UTF-8"}]]]])
+
--- src/dj/sgml.clj
+++ src/dj/sgml.clj
@@ -2,11 +2,6 @@
   (:require [dj.repl]
             [clojure.string :as cs]))
 
-;; Uses same data representation as enlive with the following additions:
-;; :!tag and :?tag
-
-;; How they will be used is still in development
-
 (declare emit)
 
 (defprotocol IEmitForm
@@ -22,7 +17,7 @@
                        attrs))))
 
 ;; TODO: add escaping
-(defn emit-tag [data]
+#_ (defn emit-tag [data]
   (if-let [tag (:tag data)]
     (let [tag:name (name tag)
           attrs:emit-pre (emit-attrs (:attrs data))
@@ -48,6 +43,45 @@
         (str "<?" (name ?tag) (emit-attrs (:attrs data)) " ?>")
         (ex-info "No tag specified" (dj.repl/local-context))))))
 
+(defn emit-tag [data]
+  (let [tag:name (name (first data))]
+    (case (first tag:name)
+      \! (case tag:name
+           "!--" (str "<!-- " (second data) " -->")
+           ;; Does not check for ]]> in content
+           "!CDATA" (str "<![CDATA[" (second data) "]]>")
+           (let [content (next data)]
+             (if content
+               (str "<" tag:name " "
+                    (cs/join " " (for [form content]
+                                   (if (vector? form)
+                                     (str "["(apply str (map emit-tag form)) "]")
+                                     (if (string? form)
+                                       (pr-str form)
+                                       (name form))))) ">")
+               (str "<" tag:name ">"))))
+      \? (str "<" tag:name " " (emit-attrs (second data)) "?>")
+      (let [[attrs & content] (rest data)]
+        (let [attrs:emit-pre (emit-attrs attrs)
+              attrs:emit (when-not (empty? attrs:emit-pre)
+                           (str " " attrs:emit-pre))]
+          (if content
+            (str "<" tag:name attrs:emit ">"
+                 (emit content)
+                 "</" tag:name ">")
+            (str "<" tag:name attrs:emit " />")))))))
+
+(comment
+  (dj.sgml/emit [[:!DOCUMENT :html
+                  [[:!DOCUMENT :html :blah]
+                   [:!DOCUMENT :html :blah]]]
+                 [:!-- "asdf falskdfj asldfkj alskfdj "]
+                 [:?asdf {:asdf "asdf"
+                          :ab "bb"}]
+                 [:body {:class "jasdf"} "use" "it"]
+                 [:body nil nil]])
+  "<!DOCUMENT html [<!DOCUMENT html blah><!DOCUMENT html blah>]><!-- asdf falskdfj asldfkj alskfdj  --><?asdf asdf=\"asdf\" ab=\"bb\"?><body class=\"jasdf\">useit</body><body></body>")
+
 (extend-type String
   IEmitForm
   (-emit-form [form]
@@ -63,11 +97,66 @@
   (-emit-form [form]
     (emit-tag form)))
 
-;; convert everything to use new form [tag attr & content]
-(defn convert [[tag attr & content]]
-  {:tag tag
-   :attr attr
-   :content (vec content)})
-
 (defn emit [forms]
   (apply str (map -emit-form forms)))
+
+;; really it should be templates and tools that let you modify subsets
+;; of the tree like first node with name 'html' etc.
+
+;; example API
+;; (update-attr [:html :head] assoc :lang "en")
+;; (update-content [:html] conj [:body nil])
+
+;; generically we need to update an element in a vector that matches
+(defn update-vector
+  [v match? f]
+  (let [size (count v)]
+       (loop [n 0]
+         (if (< n size)
+           (let [child (v n)]
+             (if (match? child)
+               (assoc v
+                 n (f child))
+               (recur (inc n))))
+           v))))
+
+(defn update-node [content [k & ks] f]
+  (let [match? #(if (vector? %)
+                  (= (first %) k)
+                  nil)]
+    (if ks
+      (update-vector content
+                     match?
+                     #(update-node % ks f))
+      (update-vector content
+                     match?
+                     f))))
+
+(defn assoc-in-attr [content nks aks v]
+  (update-node content
+               nks
+               #(assoc-in % (list* 1 aks)
+                          v)))
+
+(defn append-content [content ks new-content]
+  (update-node content
+               ks
+               #(into % new-content)))
+
+#_ (do
+     (update-vector [1 2 3 4] #(= % 2)
+                    #(+ % 1))
+     [1 3 3 4]
+
+     (update-node [[1 2 3]
+                   [2 [4] [3]]]
+                  [2 10]
+                  #(conj % 0))
+     [[1 2 3] [2 [4] [3]]]
+
+     (append-content [[1 2 3]
+                      [2 [4] [3]]]
+                     [2 4]
+                     [1 2 3 4])
+     [[1 2 3] [2 [4 1 2 3 4] [3]]]
+     )

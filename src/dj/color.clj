78b86d8924683e7cb90fd571ef3ab359bbdbd95a
Brent Millare <brent.millare@gmail.com>
2014-06-23 15:30:22 -0400

    color templates

---
+++ src/dj/color.clj
@@ -0,0 +1,29 @@
+(ns dj.color)
+
+(def basics
+  {:white "#ffffff"
+   :black "#000000"
+
+   :foreground-light-gray "#bebebe"
+   :foreground-dark-gray "#5e5e5e"
+   :background-light-gray "#252525"
+   :background-medium-gray "#191818"
+   :background-dark-gray "#111010"})
+
+;; Giant Goldfish Palette http://www.colourlovers.com/palette/92095/Giant_Goldfish
+(def giant-goldfish
+  {:aoi "#69d2e7"
+   :clean-pondwater "#a7dbd8"
+   :beach-storm "#e0e4cc"
+   :giant-goldfish "#f38630"
+   :unreal-food-pills "#fa6900"
+   :unreal-food-pills-light "#faab72"
+   :unreal-food-pills-dark "#a84700"})
+
+(def arstechnica
+  {:foreground-white "#ffffff"
+   :foreground-green "#56a73e"
+   :bright-orange "#ff6420"
+   :orange "#ff4e00"
+   :dark-orange "#ed4900"
+   :darker-orange "#bd3a00"})

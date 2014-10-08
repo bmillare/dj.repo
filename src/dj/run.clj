94c77b211510ef57f8243facb3c83f58adb32b31
Brent Millare <brent.millare@gmail.com>
2014-10-08 10:08:23 -0400

    shell

--- src/dj/run.clj
+++ src/dj/run.clj
@@ -1,9 +1,9 @@
 (ns dj.run
   (:require [dj]
             [dj.io]
-            [clojure.java.shell :as sh]))
+            [dj.shell :as ds]))
 
-(defn run-vector
+#_ (defn run-vector
   "commands is a vector of vector args
  ex. [[\"ls\" \"-l\"]]
 
@@ -16,70 +16,85 @@ log form is a vector"
                       (comp
                        (filter (complement empty?))
                        (map (fn [command]
-                              {:time (java.util.Date.)
-                               :group-time group-time
-                               :command command
-                               :future (future (apply sh/sh command))})))
+                              (merge
+                               (apply ds/proc command)
+                               {:time (java.util.Date.)
+                                :group-time group-time
+                                :command command}))))
                       commands)]
     (swap! log
            into
            entries)))
 
+(defn init-run-map []
+  {:id-gen (java.util.concurrent.atomic.AtomicInteger.)
+   :data {}})
+
+(defn nextid [^java.util.concurrent.atomic.AtomicInteger x]
+  (.getAndIncrement x))
+
 (defn run-map
   "commands is a vector of vector args
  ex. [[\"ls\" \"-l\"]]
 
-Executes all commands that are not already running
+Executes all commands given condition
 
-log form is a map, the key is the command"
-  [log commands]
+  condition? accepts command and dlog
+  tx accepts the id/addition and the new db state, it must return new state
+  on-success accepts id and proc-map
+"
+  [{:keys [log condition? tx on-success commands]}]
   (let [group-time (java.util.Date.)]
     (doseq [command commands]
       (when-not (empty? command)
         (loop []
-          (let [dlog @log]
-            (when (or (not (dlog command)) ; first run
-                      (get-in dlog [command :finish]) ; finished
-                      )
-              (let [dlog' (assoc dlog
-                            command
-                            {:time (java.util.Date.)
-                             :group-time group-time
-                             :command command
-                             :finish nil
-                             :result nil})]
+          (let [dlog @log
+                id-gen (:id-gen dlog)]
+            (when (condition? command dlog)
+              ;; If we replace this with datomic, we need a way to duplicate 'if we succeeded'
+              ;; You can either use datomic's :db.fn/cas or add an owner id and check if the id is you
+              (let [id (nextid id-gen)
+                    entry {:id id
+                           :time (java.util.Date.)
+                           :group-time group-time
+                           :command command}]
                 (if (compare-and-set! log
                                       dlog
-                                      dlog')
-                  (let [f (future (let [ret (apply sh/sh command)]
-                                    (swap! log
-                                           update-in [command]
-                                           assoc
-                                           :finish (java.util.Date.)
-                                           :result ret)
-                                    ret))]
+                                      (tx id
+                                          entry
+                                          (assoc-in dlog
+                                                    [:data id]
+                                                    entry)))
+                  (let [proc (apply ds/proc command)]
                     (swap! log
-                           (fn [dlog]
-                             (if (get-in dlog [command :finish])
-                               dlog
-                               (assoc-in dlog
-                                         [command :future]
-                                         f)))))
+                           update-in
+                           [:data id]
+                           merge
+                           proc)
+                    (on-success id proc))
                   (recur))))))))))
 
-(def clear-done (map (fn [entry]
-                       (if-let [future-entry (:future entry)]
-                         (if (future-done? future-entry)
-                           (dissoc entry :future)
-                           entry)
-                         entry))))
-;; doesn't seem to stop the process
-(def cancel (map (fn [entry]
-                   (if-let [future-entry (:future entry)]
-                     (future-cancel future-entry)
-                     entry))))
-(def running (filter (fn [entry]
-                       (not (future-done? (:future entry))))))
+(defn stringify [log]
+  (fn [id proc]
+    (let [out (future (ds/stream-to-string proc :out))
+          err (future (ds/stream-to-string proc :err))
+          exit-code (future (ds/exit-code proc))]
+      (future (swap! log
+                     update-in
+                     [:data id]
+                     assoc
+                     :out-str @out
+                     :err-str @err
+                     :exit-code @exit-code
+                     :finished (java.util.Date.))))))
+
+(def destroy (map (fn [entry]
+                    (if (:process entry)
+                      (do (ds/destroy entry)
+                          entry)
+                      entry))))
+(def running? (filter (fn [entry]
+                       (not (:finished entry)))))
 (defn has-args [& check-args]
   (filter (fn [entry]
             (every? (fn [^String check-arg]

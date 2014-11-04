8bbb9110b61492695f106e506841c598d3880797
Brent Millare <brent.millare@gmail.com>
2014-11-04 13:38:32 -0500

    typo

--- src/dj/repo/init.clj
+++ src/dj/repo/init.clj
@@ -5,6 +5,6 @@
 (defn init-sys []
   {:dj/repositories (merge cemerick.pomegranate.aether/maven-central
                            {"clojars" "http://clojars.org/repo"})
-   :dj/packages (dj.repo/default-packages}))
+   :dj/packages (dj.repo/default-packages)})
 
 (def sys (init-sys))

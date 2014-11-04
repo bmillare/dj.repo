3d1d10171fdb172065f955f53ef5c067d402a3b1
Brent Millare <brent.millare@gmail.com>
2014-11-04 13:17:49 -0500

    scripting profile support

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.4"]]
-  :profiles {:scripting {:dependencies [[org.clojure/clojure "1.6.0"]
+  :profiles {:scripting {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                                         [org.bmillare/dj.project "0.3.1"]]}}                 )
 
 

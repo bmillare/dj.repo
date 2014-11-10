7993da7f1dd49cb3bcd5a500ec82340d69a7faf7
Brent Millare <brent.millare@gmail.com>
2014-11-10 15:49:39 -0500

    peg and treefn

--- src/dj/compose/treefn.clj
+++ src/dj/compose/treefn.clj
@@ -4,9 +4,22 @@
             [dj.compose.algorithm :as dca]))
 
 (defmacro fn
-  "adds dependency metadata to fn"
+  "adds dependency metadata to fn
+
+  Use this only with treefn"
   [bindings & body]
   `(with-meta (clojure.core/fn ~bindings
+                ~@body)
+     {:dj.compose {:dependencies '~(mapv keyword bindings)}}))
+
+(defmacro fm
+  "adds dependency metadata to fn, uses map destructuring
+assume you provide the vector in {:keys [a b ...]}
+
+  Use this only with treefm
+  "
+  [bindings & body]
+  `(with-meta (clojure.core/fn [{:keys ~bindings}]
                 ~@body)
      {:dj.compose {:dependencies '~(mapv keyword bindings)}}))
 
@@ -25,7 +38,7 @@ Takes a fn-or-var and a vector of keys
 (declare ^:dynamic eval-pass)
 
 ;; fastest, fully inlined, best for inner loops, generation time argument checking
-(defn eval-emitter [shaken-map
+(defn inlined-emitter [shaken-map
                     symbols
                     shaken-keys
                     input-keys
@@ -125,7 +138,7 @@ Takes a fn-or-var and a vector of keys
 
 (defn treefn
   ([fn-map root-key input-keys]
-     (treefn* fn-map root-key input-keys eval-emitter))
+     (treefn* fn-map root-key input-keys inlined-emitter))
   ([fn-map root-key input-keys emitter]
      (let [input-key-set (set input-keys)
            available-keys (set (keys fn-map))
@@ -160,6 +173,7 @@ Takes a fn-or-var and a vector of keys
                                                    (set dependents))))
                                              {}
                                              shaken-map)
+           ;; remove input-keys so we know what functions to call
            shaken-dag (reduce-kv (clojure.core/fn [ret k dependents]
                                    (assoc ret
                                      k
@@ -181,6 +195,82 @@ Takes a fn-or-var and a vector of keys
                            input-keys
                            sorted-keys
                            root-key)
+         {:dj.compose {:dag shaken-dag-with-inputs}}))))
+
+(defn treefm
+  "shakes fm-map with root-key and produces a treefn but with fm instead of fn, which uses map destructuring
+
+  Internally, maps are being passed around to the functions"
+  ([fn-map root-key]
+     (treefm fn-map root-key false))
+  ([fn-map root-key return-val-map?]
+     (let [available-keys (set (keys fn-map))
+           ;; set of keys
+           shaken-keys ((clojure.core/fn collect [all-dependents temp-key]
+                          (let [the-fn (temp-key fn-map)
+                                dependents (-> the-fn
+                                               meta
+                                               :dj.compose
+                                               :dependencies
+                                               set)]
+                            (if (empty? dependents)
+                              (conj all-dependents
+                                    temp-key)
+                              (reduce collect
+                                      (cs/union dependents all-dependents)
+                                      dependents))))
+                        #{root-key}
+                        root-key)
+           ;; set of keys
+           input-key-set (cs/difference shaken-keys available-keys)
+           ;; map of keys->fms that will be called
+           shaken-map (select-keys fn-map shaken-keys) ; select-keys automatically disregards keys 
+           ;; map of keys->keys (also for analysis, contains topological information)
+           shaken-dag-with-inputs (reduce-kv (clojure.core/fn [ret k the-fn]
+                                               (let [dependents (-> the-fn
+                                                                    meta
+                                                                    :dj.compose
+                                                                    :dependencies)]
+                                                 (assoc ret
+                                                   k
+                                                   (set dependents))))
+                                             {}
+                                             shaken-map)
+           ;; map of keys->keys (remove input-keys so we call only functions)
+           shaken-dag (reduce-kv (clojure.core/fn [ret k dependents]
+                                   (assoc ret
+                                     k
+                                     (cs/difference dependents
+                                                    input-key-set)))
+                                 {}
+                                 shaken-dag-with-inputs)
+           ;; vector of keys of fms we will call
+           sorted-keys (dca/topological-sort shaken-dag)]
+       (with-meta (clojure.core/fn [partial-val-map]
+                    (let [provided-input-key-set (set (keys partial-val-map))]
+                      (when-not (empty? (cs/difference input-key-set provided-input-key-set))
+                        (throw (ex-info "provided input keys does not cover required inputs"
+                                        {:required-input-key-set input-key-set
+                                         :provided-input-key-set provided-input-key-set}))))
+                    (let [ret-val-map (persistent!
+                                       (reduce (fn [val-map the-fn-key]
+                                                 ;; we can reuse previously computed values
+                                                 (if (val-map the-fn-key)
+                                                   val-map
+                                                   (let [the-fn (shaken-map the-fn-key)]
+                                                     (assoc! val-map
+                                                             the-fn-key
+                                                             (try
+                                                               (the-fn val-map)
+                                                               (catch Exception e
+                                                                 (throw (ex-info "treefn node error"
+                                                                                 (persistent! val-map)
+                                                                                 e))))))))
+                                               (transient partial-val-map)
+                                               sorted-keys))]
+                      (if return-val-map?
+                        ret-val-map
+                        (ret-val-map root-key))))
          {:dj.compose {:dag shaken-dag-with-inputs}}))))
 
 #_ (do
@@ -208,7 +298,14 @@ Takes a fn-or-var and a vector of keys
                                             dj.compose.treefn/partial-emitter)]
        (let-fn {:n 0 :d 1 :b 10}))
 
-
+     (let [m {:c (fm [n]
+                     (* 3 n))
+              :a (fm [c]
+                     (inc c))
+              :b (fm [a c d]
+                     (+ a c d))}
+           tfm (treefm m :b true)]
+       (tfm {:n 0 :d 1}))
 
 
      (ex-data (.getCause clojure.core/*e)))

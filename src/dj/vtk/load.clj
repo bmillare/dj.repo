bf7816a17d00bdf0145dd9066a83b98c5740112f
Brent Millare <brent.millare@gmail.com>
2014-11-28 13:30:26 -0500

    dynamic checking for loading vtk

--- src/dj/vtk/load.clj
+++ src/dj/vtk/load.clj
@@ -5,18 +5,31 @@
   "#for arch\nsudo pacman -Syu vtk")
 
 (defn load []
-  (dj.classloader/add-classpath "/usr/share/java/vtk/vtk.jar")
-  (dj.classloader/append-native-path! [(-> (into []
-                                                 (comp
-                                                  (map dj.io/get-path)
-                                                  (filter #(re-find #"vtk" %)))
-                                                 (dj.io/ls (dj.io/file "/usr/lib64")))
-                                           first)]) ; ex: "/usr/lib64/vtk-5.10"
-  (doseq [l ["vtkCommonJava"
-             "vtkFilteringJava"
-             "vtkIOJava"
-             "vtkImagingJava"
-             "vtkGraphicsJava"
-             "vtkRenderingJava"]]
-    (clojure.lang.RT/loadLibrary l)))
+  (let [;; ex: "/usr/lib64/vtk-5.10"
+        native-library-directory-path (-> (into []
+                                                (comp
+                                                 (map dj.io/get-path)
+                                                 (filter #(re-find #"vtk" %)))
+                                                (dj.io/ls (dj.io/file "/usr/lib64")))
+                                          first)
+        native-library-names (into []
+                                   (comp
+                                    (filter #(re-find #"Java\.so$" (dj.io/get-path %)))
+                                    (map dj.io/get-name)
+                                    (map (fn [s]
+                                           (second (re-matches #"lib(vtk.+?Java)\.(so|dll)" s)))))
+                                   (dj.io/ls (dj.io/file "/usr/lib64/vtk-5.10")))]
+    (dj.classloader/add-classpath "/usr/share/java/vtk/vtk.jar")
+    (dj.classloader/append-native-path! [native-library-directory-path])
+    
+    (doseq [l native-library-names]
+      (clojure.lang.RT/loadLibrary l))))
+
+
+
+
+
+
+
+
 
